c92d003be4f879ffefb7503926514d5e1ac46284
CullenM <36140505+Heimdaillr@users.noreply.github.com>
2021-02-12 14:30:37 -0500

    Update AddTenController.cs

--- HTTP5101_Assignment1/Controllers/AddTenController.cs
+++ HTTP5101_Assignment1/Controllers/AddTenController.cs
@@ -7,6 +7,14 @@ using System.Web.Http;
 
 namespace HTTP5101_Assignment1.Controllers
 {
+///<summary>
+///Had trouble initiall settping up an Api Controller, default for program is MVC controller.
+///Basic addition from the input of the user, referred to class notes 
+///Initial attempt below, fixed the miscall for a returned string
+/// public string Get(id)
+///      {return int(id +10); }
+///</summary>
+///<parameter> id </parameter>
     public class AddTenController : ApiController
     {
         //GET api/addten/id
@@ -16,10 +24,3 @@ namespace HTTP5101_Assignment1.Controllers
         }
     }
 }
-///<summary>
-///Had trouble initiall settping up an Api Controller, default for program is MVC controller.
-///Basic addition from the input of the user, referred to class notes 
-///Initial attempt below, fixed the miscall for a returned string
-/// public string Get(id)
-///      {return int(id +10); }
-///</summary>
